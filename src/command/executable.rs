46bd3203d5d086297c0d45bb930bf169b1f8d306
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2025-07-26 15:36:57 +0200

    refactor: simplify `ExecutableCommand` trait

--- src/command/executable.rs
+++ src/command/executable.rs
@@ -2,5 +2,5 @@ pub trait PersistenceClient: Send + Sync + 'static {}
 
 pub trait ExecutableCommand<TClient: PersistenceClient + ?Sized> {
     type Output;
-    fn execute<'a>(self, client: &'a TClient) -> impl Future<Output = Self::Output> + Send + 'a;
+    fn execute(self, client: &TClient) -> impl Future<Output = Self::Output> + Send;
 }
